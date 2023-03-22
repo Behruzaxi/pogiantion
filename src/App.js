2e764af9518a609cadda16ae727eb8e57078fb86
Behruzaxi <ebehruz846@gmail.com>
2023-03-22 16:41:17 +0500

    fds

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactPaginate from "react-paginate";
+import { AiFillLeftCircle, AiFillRightCircle } from "react-icons/ai";
+import { IconContext } from "react-icons";
+import { useEffect, useState } from "react";
+import "./stil.css"
+const data = [
+  "Uzbekistan",
+  "Tadjikistan",
+  "Qazakhstan",
+  "Qirgiziston",
+  "Russia",
+  "Germany",
+  "USA",
+  "BRITAIN",
+  "France",
+  "China",
+  "Korea",
+  "Indonesia",
+  "Malayzia",
+  "Africa",
+  "Japanese",
+  "Brazil",
+  "Canada",
+  "Iraq",
+  "Austria",
+  "Turkey",
+  "Urugvay",
+  "Vietnam",
+  "Italy",
+  "Australia",
+  "Spain",
+  "Portugal"
+];
 
-function App() {
+export default function App() {
+  const [page, setPage] = useState(0);
+  const [filterData, setFilterData] = useState();
+  const n = 4;
+
+  useEffect(() => {
+    setFilterData(
+      data.filter((item, index) => {
+        return (index >= page * n) & (index < (page + 1) * n);
+      })
+    );
+  }, [page]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {filterData && filterData.map((item, index) => <li>Country : {item}</li>)}
+      </ul>
+      <ReactPaginate
+        containerClassName={"pagination"}
+        activeClassName={"active"}
+        pageClassName={"page-item"}
+        onPageChange={(event) => setPage(event.selected)}
+        breakLabel="..."
+        pageCount={Math.ceil(data.length / n)}
+        previousLabel={
+          <IconContext.Provider value={{ color: "#B8C1CC", size: "36px" }}>
+            <AiFillLeftCircle />
+          </IconContext.Provider>
+        }
+        nextLabel={
+          <IconContext.Provider value={{ color: "#B8C1CC", size: "36px" }}>
+            <AiFillRightCircle />
+          </IconContext.Provider>
+        }
+      />
     </div>
   );
 }
-
-export default App;
